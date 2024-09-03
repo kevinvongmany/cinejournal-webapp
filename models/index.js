0a9dcbaf345677d99a4f4296006ceef0263bb21e
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-09-03 21:42:08 +1000

    Set up relationships between files and exported each model

--- models/index.js
+++ models/index.js
@@ -1,13 +1,11 @@
 const User = require('./User');
-const Project = require('./Project');
+const Entry = require('./Entry');
+const Platform = require('./Platform');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+User.hasMany(Entry, { foreignKey: 'user_id' });
+Entry.belongsTo(User, { foreignKey: 'user_id' });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+Platform.hasMany(Entry, { foreignKey: 'platform_id' });
+Entry.belongsTo(Platform, { foreignKey: 'platform_id' });
 
-module.exports = { User, Project };
+module.exports = { User, Entry, Platform };
