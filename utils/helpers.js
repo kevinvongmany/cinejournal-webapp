d97c3e7fc0f5fb4e9928a0daeaca3aefab53f16a
Hank Star <kate.stapley@hotmail.com>
2024-09-10 19:31:22 +1000

    date format correction

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,17 @@
 const moment = require('moment');
 
-
 module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
     // use moment.js to convert this date format 2024-09-05 19:06:48.84+10 to 09/05/2024
-    return moment(date).format('MM/DD/YYYY');
+    return moment(date).format('DD/MM/YYYY');
   },
+
+  parse_date: (date) => {
+  // use moment.js to convert this date format 13/05/2024 to 2024-05-13 19:06:48.84+10
+  return moment(date, 'DD/MM/YYYY').format('YYYY-MM-DD');
+  },
+
   format_amount: (amount) => {
     // format large numbers with commas
     return parseInt(amount).toLocaleString();
