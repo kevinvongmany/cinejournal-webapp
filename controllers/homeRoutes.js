b7803758fbd95df4250f2543a5d2d8defdf07f90
Hank Star <kate.stapley@hotmail.com>
2024-09-12 19:20:18 +1000

    platform name parsed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,78 +1,83 @@
-const router = require('express').Router();
-const { User, Platform, Entry } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Platform, Entry } = require("../models");
+const withAuth = require("../utils/auth");
 
 // Home Route (GET /)
-router.get('/', withAuth,async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     // Fetch all users, platforms, and entries from the database
-    const users = await User.findByPk(req.session.user_id, { attributes: { exclude: ['password'] }, include: [{ model: Entry, include: Platform }] });
+    const users = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Entry, include: {model: Platform, attributes:['name']} }],
+    });
     const platforms = await Platform.findAll();
     const entries = await Entry.findAll();
 
     const entriesData = entries.map((entry) => entry.get({ plain: true }));
     const usersData = users.get({ plain: true });
-    console.log(usersData);
+    console.log(usersData.entries);
     // Render a combined JSON response with all data
-    res.render('watchlist', {
+    res.render("watchlist", {
       entriesData,
-      logged_in: req.session.loggedIn 
+      logged_in: req.session.loggedIn,
     });
-  } 
-  catch (err) {
-    res.status(500).json({ message: 'Failed to load home data', error: err });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to load home data", error: err });
   }
 });
 
-
-router.get('/login', async (req, res) => {
+router.get("/login", async (req, res) => {
   try {
-    res.render('login');
-  } 
-  catch (err) {
-    res.status(500).json({ message: 'Failed to load login page', error: err });
+    res.render("login");
+  } catch (err) {
+    res.status(500).json({ message: "Failed to load login page", error: err });
   }
 });
 
-router.get('/register', async (req, res) => {
+router.get("/register", async (req, res) => {
   try {
-    res.render('register');
-  } 
-  catch (err) {
-    res.status(500).json({ message: 'Failed to load register page', error: err });
+    res.render("register");
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to load register page", error: err });
   }
 });
 
-router.get('/form', async (req, res) => {
+router.get("/form", async (req, res) => {
   try {
     // Fetch platform data
     const platformData = await Platform.findAll();
-    const platforms = platformData.map((platform) => platform.get({ plain: true }));
+    const platforms = platformData.map((platform) =>
+      platform.get({ plain: true })
+    );
 
-    res.render('form', {
+    res.render("form", {
       platforms,
-      logged_in: req.session.loggedIn 
+      logged_in: req.session.loggedIn,
     });
-  } 
-  catch (err) {
-    res.status(500).json({ message: 'Failed to load form page', error: err });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to load form page", error: err });
   }
 });
 
-router.get('/watchlist', async (req, res) => {
+router.get("/watchlist", async (req, res) => {
   try {
     // Fetch platform data from the database
     const platformData = await Platform.findAll();
-    const platforms = platformData.map((platform) => platform.get({ plain: true }));
+    const platforms = platformData.map((platform) =>
+      platform.get({ plain: true })
+    );
 
     // Render the watchlist page and pass the platform data
-    res.render('watchlist', {
+    res.render("watchlist", {
       platforms,
-      logged_in: req.session.loggedIn
+      logged_in: req.session.loggedIn,
     });
-  } 
-  catch (err) {
-    res.status(500).json({ message: 'Failed to load watchlist page', error: err });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to load watchlist page", error: err });
   }
 });
 
